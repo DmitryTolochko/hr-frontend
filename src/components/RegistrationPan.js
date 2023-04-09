9be2c4ab71ccc39b55a2b64b9d5fa2ac9c7592d8
Elizaveta248 <Elizaveta.Shmeleva@urfu.me>
2023-04-09 23:11:53 +0500

    страница регистрации + правки логина

--- src/components/RegistrationPan.js
+++ src/components/RegistrationPan.js
@@ -2,29 +2,31 @@ import React from 'react';
 
 const Registration = () => {
     return (
-        <div className='login-pan'>
-            <div id = 'circle'>
-                <h1 className='header-1'>
+        <div className='glav_registrationPan'>
+            <div className='registrationPan'>
+                <h1 className='registrationPan_h1'>
                     Регистрация 
                 </h1>
+                <div className = 'circle2'>
+                </div>
+                <input className = 'registrationPan_first_input' placeholder='ФИО'>
+                </input>
+                <input className = 'registrationPan_second_input' placeholder='Почта'>
+                </input>
+                <input className = 'registrationPan_third_input' placeholder='Телефон'>
+                </input>
+                <input className = 'registrationPan_four_input' placeholder='Пароль'>
+                </input>
+                <button className='registrationPan_button'>
+                    Зарегестрироваться
+                </button>
+                <div className='registrationPan_div'>
+                    Уже есть аккаунт?&nbsp;
+                    <a className='registrationPan_a' href = ''> 
+                        Вход
+                    </a>
+                </div>
             </div>
-            <input className = 'input' placeholder='ФИО'>
-            </input>
-            <input className = 'input' placeholder='Почта'>
-            </input>
-            <input className = 'input' placeholder='Телефон'>
-            </input>
-            <input className = 'input' placeholder='Пароль'>
-            </input>
-            <button className='input-button'>
-                Зарегестрироваться
-            </button>
-            <p>
-                Уже есть аккаунт?
-                <a> 
-                    Вход
-                </a>
-            </p>
         </div>
     );
 };
