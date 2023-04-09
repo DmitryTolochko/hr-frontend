cbb3286e6905b08585d399affa6de5fbafc023a1
Elizaveta248 <Elizaveta.Shmeleva@urfu.me>
2023-04-09 21:30:39 +0500

    правки логин

--- src/components/LoginPan.js
+++ src/components/LoginPan.js
@@ -2,22 +2,24 @@ import React from 'react';
 
 const LoginPan = () => {
     return (
-        <div className='loginPan'>
-            <h1 className='loginPan_h1'>
-                    Вход 
-            </h1>
-            <div id = 'circle'>
-            </div>
-            <input className = 'loginPan_first_input' placeholder='Почта или телефон'/>
-            <input className = 'loginPan_second_input' placeholder='Пароль'/>
-            <button className='loginPan_button'>
-                Войти
-            </button>
-            <div className='loginPan_div'>
-                Еще нет аккаунта?&nbsp;
-                <a className='loginPan_a' href = ''> 
-                    Регистрация
-                </a>
+        <div className='glav_loginPan'>
+            <div className='loginPan'>
+                <h1 className='loginPan_h1'>
+                        Вход 
+                </h1>
+                <div id = 'circle'>
+                </div>
+                <input className = 'loginPan_first_input' placeholder='Почта или телефон'/>
+                <input className = 'loginPan_second_input' placeholder='Пароль'/>
+                <button className='loginPan_button'>
+                    Войти
+                </button>
+                <div className='loginPan_div'>
+                    Еще нет аккаунта?&nbsp;
+                    <a className='loginPan_a' href = ''> 
+                        Регистрация
+                    </a>
+                </div>
             </div>
         </div>
     );
